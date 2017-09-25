6f0e837ff313f108b9d20fa7f9f8d0a368d46217
teodyseguin@gmail.com <teodyseguin@gmail.com>
2017-09-26 07:41:14 +0800

    refactor(skyword): extend the base controller class

--- skyword-7.x/src/Controller/BaseController.php
+++ skyword-7.x/src/Controller/BaseController.php
@@ -1,6 +1,44 @@
 <?php
 
 class BaseController {
+  /**
+   * Build the data normally
+   *
+   * @param $taxonomies
+   *   an array of taxonomies
+   */
+  protected function buildData($entities, $fields, $list = TRUE) {
+    if ($list) {
+      $data = [];
+
+      foreach ($entities as $entity) {
+        $obj = new stdClass();
+
+        foreach ($fields as $field) {
+          $obj->{$field} = $entity->{$field};
+        }
+
+        $data[] = $obj;
+      }
+
+      return $data;
+    }
+    else {
+      $obj = new stdClass();
+
+      foreach ($fields as $field) {
+        $obj->{$field} = $entities->{$field};
+      }
+
+      return $obj;
+    }
+  }
+
+  protected function extractFields($fields) {
+    $f = explode(',', $fields);
+    return array_flip($f);
+  }
+
   protected function limitOutputByFields($fields, &$data) {
     $f = explode(',', $fields);
     $fieldsOutput = array_flip($f);
@@ -14,3 +52,4 @@ class BaseController {
     }
   }
 }
+
