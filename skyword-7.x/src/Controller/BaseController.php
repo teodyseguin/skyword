43911e40d817557114d562e17a9dc8171dfeec73
teodyseguin@gmail.com <teodyseguin@gmail.com>
2017-10-09 19:31:41 +0800

    refactor(skyword): fix the issue on the record visible per page

--- skyword-7.x/src/Controller/BaseController.php
+++ skyword-7.x/src/Controller/BaseController.php
@@ -39,8 +39,8 @@ class BaseController {
       drupal_add_http_header('LINK', "<{$url}?page={$prev}&per_page={$per_page}>; rel=\"prev\"", TRUE);
     }
 
-    if ($first_record >= $per_page) {
-      $this->query->range(0, $per_page);
+    if ($per_page > $total) {
+      $this->query->range(0, $total);
     }
     else {
       $this->query->range($first_record, $per_page);
